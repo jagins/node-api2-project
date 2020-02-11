dade8b3da418b614abb6f5e811078d0d3f12a822
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-11 14:17:46 -0600

    get all posts works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,5 +1,20 @@
 const express = require("express");
 
+const database = require("../data/db");
+
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  database
+    .find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved" });
+    });
+});
 
 module.exports = router;
