98504e59161c0cbe3981d04ebcd36bdadd03cf99
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-11 14:11:51 -0600

    made a posts router and brought it into index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 
+const postsRouter = require("./posts/posts-router");
+
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/posts", postsRouter);
 
 server.get("/", (req, res) => {
   res.json({ message: "api is on" });
