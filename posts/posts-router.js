2e310c6d51316d300da06938ddc29b07b21a0737
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-02-11 18:13:39 -0600

    put request kinda works

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -120,7 +120,45 @@ router.post("/:id/comments", (req, res) => {
     );
 });
 
-router.put("/:id", (req, res) => {});
+// PUT /api/posts/:id
+router.put("/:id", (req, res) => {
+  database
+    .findById(req.body.id)
+    .then(posts => {
+      if (posts !== 0) {
+        if (!req.body.title || !req.body.contents) {
+          res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+          });
+        } else {
+          database
+            .update(req.body.id, req.body)
+            .then(didUpdate => {
+              const updatedPost = {
+                title: req.body.title,
+                contents: req.body.contents
+              };
+
+              res.status(201).json(updatedPost);
+            })
+            .catch(error => {
+              res
+                .status(500)
+                .json({ error: "The post information could not be modified." });
+            });
+        }
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist" });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved" });
+    });
+});
 
 //DELETE /api/posts/:id
 router.delete("/:id", (req, res) => {
